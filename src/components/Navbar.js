f1b5fbc972a27736e39178e996bd69ddce6b58fe
TSKHayagreevaC <srikanth.thiru12@gmail.com>
2024-02-05 07:25:23 +0530

    feat: Added initial framework

---
+++ src/components/Navbar.js
@@ -0,0 +1,39 @@
+// src/components/Navbar.js
+
+import { HomeIcon } from "@heroicons/react/solid";
+import React from "react";
+
+export default function Navbar() {
+  return (
+    <header className="bg-gray-800 md:sticky top-0 z-10">
+      <div className="container mx-auto flex flex-wrap p-5 flex-col md:flex-row items-center">
+        <a
+          href="#about"
+          className="title-font font-medium text-white mb-4 md:mb-0"
+        >
+          <span className="flex flex-row align-items-center justify-items-center">
+            <HomeIcon className="w-8 h-5 ml-1" />
+            Home
+          </span>
+        </a>
+        <nav className="md:mr-auto md:ml-4 md:py-1 md:pl-4 md:border-l md:border-gray-700	flex flex-wrap items-center text-base justify-center">
+          <a href="#projects" className="mr-5 hover:text-white">
+            Projects
+          </a>
+          <a href="#skills" className="mr-5 hover:text-white">
+            Skills
+          </a>
+          <a href="#testimonials" className="mr-5 hover:text-white">
+            Testimonials
+          </a>
+          <a
+            href="#contact"
+            className="inline-flex items-center bg-gray-800 border-0 focus:outline-none hover:bg-gray-700 rounded text-base mt-4 md:mt-0"
+          >
+            Contact
+          </a>
+        </nav>
+      </div>
+    </header>
+  );
+}
---
+++ src/data.js
@@ -0,0 +1,46 @@
+// src/data.js
+
+export const projects = [
+  {
+    title: "React Reserve",
+    subtitle: "MERN Stack",
+    description:
+      "Lorem ipsum dolor sit amet consectetur adipisicing elit. Praesentium dolore rerum laborum iure enim sint nemo omnis voluptate exercitationem eius?",
+    image: "https://api-frameworks.vercel.sh/framework-logos/ember.svg",
+    link: "https://vercel.com/tskhayagreevac/ember-app",
+  },
+  {
+    title: "React Tracks",
+    subtitle: "React and Python",
+    description:
+      "Lorem ipsum dolor sit amet consectetur adipisicing elit. Praesentium dolore rerum laborum iure enim sint nemo omnis voluptate exercitationem eius?",
+    image: "https://api-frameworks.vercel.sh/framework-logos/svelte.svg",
+    link: "https://reedbarger.com",
+  },
+  {
+    title: "DevChat",
+    subtitle: "React and Firebase",
+    description:
+      "Lorem ipsum dolor sit amet consectetur adipisicing elit. Praesentium dolore rerum laborum iure enim sint nemo omnis voluptate exercitationem eius?",
+    image: "./project-3.gif",
+    link: "https://jsbootcamp.com",
+  },
+  {
+    title: "Epic Todo App",
+    subtitle: "React Hooks",
+    description:
+      "Lorem ipsum dolor sit amet consectetur adipisicing elit. Praesentium dolore rerum laborum iure enim sint nemo omnis voluptate exercitationem eius?",
+    image: "./project-4.gif",
+    link: "https://pythonbootcamp.com",
+  },
+];
+
+export const skills = [
+  "Java Script", 
+  "React JS"
+]
+
+export const testimonials = [
+  "testimonial one", 
+  "testimonial two"
+]

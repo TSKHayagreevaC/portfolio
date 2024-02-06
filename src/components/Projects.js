b4542e3ea89da3a007ed41a3bb96ce37da352da2
TSKHayagreevaC <srikanth.thiru12@gmail.com>
2024-02-06 07:06:08 +0530

    feat: links to project cards.

--- src/components/Projects.js
+++ src/components/Projects.js
@@ -3,15 +3,17 @@
 import { CodeIcon } from "@heroicons/react/solid";
 import React from "react";
 import { projects } from "../data";
+import { CloudIcon } from "@heroicons/react/solid";
+import { ExternalLinkIcon } from "@heroicons/react/solid";
 
 export default function Projects() {
   return (
     <section id="projects" className="text-gray-400 bg-gray-900 body-font">
-      <div className="container px-5 py-10 mx-auto text-center lg:px-40">
-        <div className="flex flex-col w-full mb-20">
+      <div className="container px-5 py-10 mx-auto text-center">
+        <div className="flex flex-col w-full mb-10">
           <CodeIcon className="mx-auto inline-block w-10 mb-4" />
           <h1 className="sm:text-4xl text-3xl font-medium title-font mb-4 text-white">
-            Apps I've Built
+            Apps I've Worked
           </h1>
           <p className="lg:w-2/3 mx-auto leading-relaxed text-base">
             Lorem ipsum, dolor sit amet consectetur adipisicing elit. Explicabo
@@ -19,31 +21,42 @@ export default function Projects() {
             fuga dolore.
           </p>
         </div>
-        <div className="flex m-4">
+        <div className="flex flex-wrap container p-5 mx-auto text-center m-4">
           {projects.map((project) => (
-            <a
-              href={project.link}
+            // <a
+            //   href={project.link}
+            //   key={project.image}
+            //   className="m-1"
+            // >
+            <div
               key={project.image}
-              className="sm:w-1/2 w-100 p-4 m-1"
+              className="p-2 m-1 border-4 rounded-lg border-gray-800 bg-gray-900"
             >
-              <div className="flex relative">
-                <div className="px-8 py-10 relative w-full border-4 border-gray-800 bg-gray-900">
-                    <img
-                      alt="gallery"
-                      className="relative inset-0 w-full h-full object-cover object-center"
-                      href={project.image}
-                      src={project.image}
-                    />
-                  {/* <h2 className="tracking-widest text-sm title-font font-medium text-green-400 mb-1">
+              <div className="mb-2 flex flex-row align-center justify-between">
+                <a className="w-full" href="/">
+                 <CodeIcon className="max-w-5" />
+                </a>
+                <a className="w-full" href="/">
+                <ExternalLinkIcon className="max-w-5" />
+                </a>
+              </div>
+              <div className="relative">
+                <img
+                  alt="gallery"
+                  className="relative inset-0 w-50 h-50 object-cover object-center"
+                  href={project.image}
+                  src={project.image}
+                />
+                {/* <h2 className="tracking-widest text-sm title-font font-medium text-green-400 mb-1">
                     {project.subtitle}
                   </h2>
                   <h1 className="title-font text-lg font-medium text-white mb-3">
                     {project.title}
                   </h1>
                   <p className="leading-relaxed">{project.description}</p> */}
-                </div>
               </div>
-            </a>
+            </div>
+            // </a>
           ))}
         </div>
       </div>
--- src/data.js
+++ src/data.js
@@ -10,28 +10,140 @@ export const projects = [
     link: "https://vercel.com/tskhayagreevac/ember-app",
   },
   {
+    title: "React Reserve",
+    subtitle: "MERN Stack",
+    description:
+      "Lorem ipsum dolor sit amet consectetur adipisicing elit. Praesentium dolore rerum laborum iure enim sint nemo omnis voluptate exercitationem eius?",
+    image: "https://api-frameworks.vercel.sh/framework-logos/ember.svg",
+    link: "https://vercel.com/tskhayagreevac/ember-app",
+  },
+  {
+    title: "React Reserve",
+    subtitle: "MERN Stack",
+    description:
+      "Lorem ipsum dolor sit amet consectetur adipisicing elit. Praesentium dolore rerum laborum iure enim sint nemo omnis voluptate exercitationem eius?",
+    image: "https://api-frameworks.vercel.sh/framework-logos/ember.svg",
+    link: "https://vercel.com/tskhayagreevac/ember-app",
+  },
+  {
+    title: "React Reserve",
+    subtitle: "MERN Stack",
+    description:
+      "Lorem ipsum dolor sit amet consectetur adipisicing elit. Praesentium dolore rerum laborum iure enim sint nemo omnis voluptate exercitationem eius?",
+    image: "https://api-frameworks.vercel.sh/framework-logos/ember.svg",
+    link: "https://vercel.com/tskhayagreevac/ember-app",
+  },
+  {
+    title: "React Reserve",
+    subtitle: "MERN Stack",
+    description:
+      "Lorem ipsum dolor sit amet consectetur adipisicing elit. Praesentium dolore rerum laborum iure enim sint nemo omnis voluptate exercitationem eius?",
+    image: "https://api-frameworks.vercel.sh/framework-logos/ember.svg",
+    link: "https://vercel.com/tskhayagreevac/ember-app",
+  },
+  {
+    title: "React Reserve",
+    subtitle: "MERN Stack",
+    description:
+      "Lorem ipsum dolor sit amet consectetur adipisicing elit. Praesentium dolore rerum laborum iure enim sint nemo omnis voluptate exercitationem eius?",
+    image: "https://api-frameworks.vercel.sh/framework-logos/ember.svg",
+    link: "https://vercel.com/tskhayagreevac/ember-app",
+  },
+  {
+    title: "React Reserve",
+    subtitle: "MERN Stack",
+    description:
+      "Lorem ipsum dolor sit amet consectetur adipisicing elit. Praesentium dolore rerum laborum iure enim sint nemo omnis voluptate exercitationem eius?",
+    image: "https://api-frameworks.vercel.sh/framework-logos/ember.svg",
+    link: "https://vercel.com/tskhayagreevac/ember-app",
+  },
+  {
+    title: "React Reserve",
+    subtitle: "MERN Stack",
+    description:
+      "Lorem ipsum dolor sit amet consectetur adipisicing elit. Praesentium dolore rerum laborum iure enim sint nemo omnis voluptate exercitationem eius?",
+    image: "https://api-frameworks.vercel.sh/framework-logos/ember.svg",
+    link: "https://vercel.com/tskhayagreevac/ember-app",
+  },
+  {
+    title: "React Reserve",
+    subtitle: "MERN Stack",
+    description:
+      "Lorem ipsum dolor sit amet consectetur adipisicing elit. Praesentium dolore rerum laborum iure enim sint nemo omnis voluptate exercitationem eius?",
+    image: "https://api-frameworks.vercel.sh/framework-logos/ember.svg",
+    link: "https://vercel.com/tskhayagreevac/ember-app",
+  },
+  {
+    title: "React Reserve",
+    subtitle: "MERN Stack",
+    description:
+      "Lorem ipsum dolor sit amet consectetur adipisicing elit. Praesentium dolore rerum laborum iure enim sint nemo omnis voluptate exercitationem eius?",
+    image: "https://api-frameworks.vercel.sh/framework-logos/ember.svg",
+    link: "https://vercel.com/tskhayagreevac/ember-app",
+  },
+  {
+    title: "React Reserve",
+    subtitle: "MERN Stack",
+    description:
+      "Lorem ipsum dolor sit amet consectetur adipisicing elit. Praesentium dolore rerum laborum iure enim sint nemo omnis voluptate exercitationem eius?",
+    image: "https://api-frameworks.vercel.sh/framework-logos/ember.svg",
+    link: "https://vercel.com/tskhayagreevac/ember-app",
+  },
+  {
+    title: "React Reserve",
+    subtitle: "MERN Stack",
+    description:
+      "Lorem ipsum dolor sit amet consectetur adipisicing elit. Praesentium dolore rerum laborum iure enim sint nemo omnis voluptate exercitationem eius?",
+    image: "https://api-frameworks.vercel.sh/framework-logos/ember.svg",
+    link: "https://vercel.com/tskhayagreevac/ember-app",
+  },
+  {
+    title: "React Reserve",
+    subtitle: "MERN Stack",
+    description:
+      "Lorem ipsum dolor sit amet consectetur adipisicing elit. Praesentium dolore rerum laborum iure enim sint nemo omnis voluptate exercitationem eius?",
+    image: "https://api-frameworks.vercel.sh/framework-logos/ember.svg",
+    link: "https://vercel.com/tskhayagreevac/ember-app",
+  },
+  {
+    title: "React Reserve",
+    subtitle: "MERN Stack",
+    description:
+      "Lorem ipsum dolor sit amet consectetur adipisicing elit. Praesentium dolore rerum laborum iure enim sint nemo omnis voluptate exercitationem eius?",
+    image: "https://api-frameworks.vercel.sh/framework-logos/ember.svg",
+    link: "https://vercel.com/tskhayagreevac/ember-app",
+  },
+  {
+    title: "React Reserve",
+    subtitle: "MERN Stack",
+    description:
+      "Lorem ipsum dolor sit amet consectetur adipisicing elit. Praesentium dolore rerum laborum iure enim sint nemo omnis voluptate exercitationem eius?",
+    image: "https://api-frameworks.vercel.sh/framework-logos/ember.svg",
+    link: "https://vercel.com/tskhayagreevac/ember-app",
+  },
+  {
+    title: "React Reserve",
+    subtitle: "MERN Stack",
+    description:
+      "Lorem ipsum dolor sit amet consectetur adipisicing elit. Praesentium dolore rerum laborum iure enim sint nemo omnis voluptate exercitationem eius?",
+    image: "https://api-frameworks.vercel.sh/framework-logos/ember.svg",
+    link: "https://vercel.com/tskhayagreevac/ember-app",
+  },
+  {
+    title: "React Reserve",
+    subtitle: "MERN Stack",
+    description:
+      "Lorem ipsum dolor sit amet consectetur adipisicing elit. Praesentium dolore rerum laborum iure enim sint nemo omnis voluptate exercitationem eius?",
+    image: "https://api-frameworks.vercel.sh/framework-logos/ember.svg",
+    link: "https://vercel.com/tskhayagreevac/ember-app",
+  },
+  {
     title: "React Tracks",
     subtitle: "React and Python",
     description:
       "Lorem ipsum dolor sit amet consectetur adipisicing elit. Praesentium dolore rerum laborum iure enim sint nemo omnis voluptate exercitationem eius?",
     image: "https://api-frameworks.vercel.sh/framework-logos/svelte.svg",
     link: "https://reedbarger.com",
-  },
-  {
-    title: "DevChat",
-    subtitle: "React and Firebase",
-    description:
-      "Lorem ipsum dolor sit amet consectetur adipisicing elit. Praesentium dolore rerum laborum iure enim sint nemo omnis voluptate exercitationem eius?",
-    image: "./project-3.gif",
-    link: "https://jsbootcamp.com",
-  },
-  {
-    title: "Epic Todo App",
-    subtitle: "React Hooks",
-    description:
-      "Lorem ipsum dolor sit amet consectetur adipisicing elit. Praesentium dolore rerum laborum iure enim sint nemo omnis voluptate exercitationem eius?",
-    image: "./project-4.gif",
-    link: "https://pythonbootcamp.com",
   },
 ];
 
